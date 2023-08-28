d9f39e2f1ad800b4587e1e2f5b802173736121d9
Facundo Kameyha <kameyhafacundo@gmail.com>
2023-08-28 11:32:35 -0300

    Modifico algunos detalles del login, acerca de nosotros y mi cuenta

--- src/components/view/MiCuenta.jsx
+++ src/components/view/MiCuenta.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./MiCuenta.css";
+
 function MiCuenta({ usuarioActivo }) {
   const [bandera, setBandera] = useState("");
+
   useEffect(() => {
     console.log("banana" + usuarioActivo.id);
     if (usuarioActivo.rol === true) {
@@ -16,8 +18,9 @@ function MiCuenta({ usuarioActivo }) {
   const id = usuarioActivo.id;
   const nombre = usuarioActivo.nombreUsuario;
   const email = usuarioActivo.email;
+
   return (
-    <div className="minHeader">
+    <div className="minHeader px-5 mx-5">
       <div className="mi-cuenta-container ">
         <div className="usuario-info">
           <h2>Mi Cuenta</h2>
@@ -36,12 +39,11 @@ function MiCuenta({ usuarioActivo }) {
         </div>
         <div className="botones-container">
           <Link to={`lista-de-deseos/${id}`}>
-            <Button variant="success">Mi Lista de Deseos</Button>
+            <Button variant="success px-3">Mi Lista de Deseos</Button>
           </Link>
 
           <Link to={`administrador-resenias/${id}`}>
-            {" "}
-            <Button variant="success">Administrar Reseñas</Button>
+            <Button variant="success ">Administrar Reseñas</Button>
           </Link>
         </div>
       </div>
